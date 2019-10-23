f9b1ff95fd22418ad6cea21444aeea60e19e2b7d
Pho <dotrietpho1@gmail.com>
2019-10-23 13:58:33 +0700

    Lul

--- DoAn1/Controllers/LoginController.cs
+++ DoAn1/Controllers/LoginController.cs
@@ -69,6 +69,8 @@ namespace DoAn1.Controllers
                         ViewBag.Messenge = "Yêu cầu nhập đẩy đủ thông tin!";
                         return View();
                     }
+                    if (a.password.Length <6 || a.TaiKhoan.Length<6)
+                    { }
                     //Neu password nhap k trung khop
                     if (a.password == a.ReTypedpassword)
                     {
@@ -102,6 +104,29 @@ namespace DoAn1.Controllers
             Session.RemoveAll();
             Session.Clear();
             return Redirect(Url.Content("~/"));
+        }
+
+        static bool IsLetter(char c)
+        {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+        }
+
+        static bool IsDigit(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
+
+        static bool IsSymbol(char c)
+        {
+            return c > 32 && c < 127 && !IsDigit(c) && !IsLetter(c);
+        }
+
+        static bool IsValidPassword(string password)
+        {
+            return
+               password.Any(c => IsLetter(c)) &&
+               password.Any(c => IsDigit(c)) &&
+               password.Any(c => IsSymbol(c));
         }
     }
 }
